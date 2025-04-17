7ce12c84eed6cc0c8306fd02fa01dd7e630917b5
LuizBoldrini <luizboldriniej@gmail.com>
2025-04-17 16:33:40 -0400

    Inserido ip para acesso front

--- ApiTarefas/src/index.js
+++ ApiTarefas/src/index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
-
-
