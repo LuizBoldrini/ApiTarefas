7814b801f4a0e2da7c33c8893ce529189c558325
LuizBoldrini <luizboldriniej@gmail.com>
2025-04-22 00:44:57 -0400

    Refatorado caminho das rotas para acesso

--- ApiTarefas/src/routes/conjuntoRouter.js
+++ ApiTarefas/src/routes/conjuntoRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const conjuntoController = require('../controllers/conjuntoController');
 
 router.get('/:usuarioId', conjuntoController.listar);
-router.post('/', conjuntoController.criar);
-router.put('/:id', conjuntoController.atualizar);
+router.post('/cadastrar', conjuntoController.criar);
+router.put('/editar/:id', conjuntoController.atualizar);
 router.delete('/:id', conjuntoController.deletar);
 
 module.exports = router;
--- ApiTarefas/src/routes/tarefaRouter.js
+++ ApiTarefas/src/routes/tarefaRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const tarefaController = require('../controllers/tarefaController');
 
 router.get('/:conjuntoId', tarefaController.listar);
-router.post('/', tarefaController.criar);
-router.put('/nome/:id', tarefaController.edtiarNome);
-router.put('/status/:id', tarefaController.alterarStatus);
+router.post('/cadastrar', tarefaController.criar);
+router.put('/editar/:id', tarefaController.edtiarNome);
+router.put('/editar/status/:id', tarefaController.alterarStatus);
 router.delete('/:id', tarefaController.deletar);
 
 module.exports = router;
